7d872631a366c6165b500be8cad1f78c312fbc36
Miguel Lopez Ariza <mlopez@transportationamerica.com>
2018-10-27 11:41:40 -0500

    Locales

---
+++ mai.py
@@ -0,0 +1,35 @@
+clientes = 'miguel, leronado,'
+
+def create_client(client_name):
+    global clientes
+    clientes += client_name
+    _add_comma()
+
+
+def list_clients():
+    global clientes
+    print(clientes)
+
+def _print_welcome():
+    print('WELCOME TO PLATZI VENTAS')
+    print('*' * 50)
+    print('What would you to like to do today ?')
+    print('[C]reate client')
+    print('[D]elete client')
+
+def _add_comma():
+    global clientes
+    clientes += ',' 
+
+
+if __name__ == '__main__':
+    _print_welcome()
+    command = input()
+    if command == 'C':
+        client_name = input('What is the client name? ')
+        create_client(client_name)
+        list_clients()
+    elif command == 'D':
+        pass
+    else:
+        print('Invalid command')
